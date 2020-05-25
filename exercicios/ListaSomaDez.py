cb4727db20edd4916746ff0c75b3800d79b75adc
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-05-25 16:52:08 -0300

    go

--- exercicios/ListaSomaDez.py
+++
@@ -1,9 +0,0 @@
-n = int(input('Digite o valor de n: '))
-
-vetor = [n]
-soma = 0
-
-for index in range(0, n):
-    vetor.append(int(input('Digite os números: ')))
-    soma += vetor[index]
-print(f'Soma = {soma}')
