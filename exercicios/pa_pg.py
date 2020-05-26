2515569a2ad5bff98db3b06cec6ffc4410e65824
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-05-26 15:03:41 -0300

    go

---
+++ exercicios/pa_pg.py
@@ -0,0 +1,30 @@
+
+def pa(a1, r, n):
+    somaPa = 0
+    
+    for index in range(n):
+        an = a1 + (r * index)
+        print(f'a{index + 1} = {an}')
+        somaPa += an
+        
+    return 'Soma pa = ' + str(somaPa)
+
+def pg(a1, q, n):
+    somaPg = 0
+    
+    for index in range(n):
+        an = a1 * (q ** index)
+        print(f'a{index + 1} = {an}')
+        somaPg += an
+        
+    return 'Soma pg = ' + str(somaPg)
+
+
+
+
+
+print(pa(1, 2, 5))
+
+print('-' * 20)
+
+print(pg(1, 2, 5))
