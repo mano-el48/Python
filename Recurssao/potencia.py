f003d7eea160179e4a4cea9412acdf25e08ff6e3
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-05-29 12:13:15 -0300

    go

---
+++ Recurssao/potencia.py
@@ -0,0 +1,12 @@
+
+def potencia(numero, expoente):
+    if numero == 1 or expoente == 0:
+        return 1
+    elif expoente == 1:
+        return numero
+    else:
+        return numero * potencia(numero, expoente-1)
+
+numero = int(input("Digite o primeiro numero: "))
+expoente = int(input("Digite o segundo numero: "))
+print(potencia(numero, expoente))
