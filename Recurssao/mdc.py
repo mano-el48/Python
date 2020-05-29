886b0090c71714132a84b95cb693ff3fa1ec29ba
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-05-29 13:16:57 -0300

    go

--- Recurssao/mdc.py
+++ Recurssao/mdc.py
@@ -1,19 +1,14 @@
 
 def mdc(primeiroNumero, segundoNumero):
-    if segundoNumero == 0:
-        return primeiroNumero
-        
-    return mdc(segundoNumero, primeiroNumero % segundoNumero)
+    # not segundoNumero -> segundoNumero == 0 pois 0 = False
+    return primeiroNumero if not segundoNumero else mdc(segundoNumero, primeiroNumero % segundoNumero)
 
 primeiroNumero = int(input('Digite o pimeiro numero: '))
 segundoNumero = int(input('Digite o segundo numero: '))
 print(mdc(primeiroNumero, segundoNumero))
-# mdc(1, 2)
-# mdc(2, 3)
-# mdc(6, 3)
-# mdc(9, 3)
 
-# def mdc(a, b):
-#     return a if not b else mdc(b, a % b)
-
-# print(mdc(70, 25)) # 5
+# def mdc(primeiroNumero, segundoNumero):
+#     if segundoNumero == 0:
+#         return primeiroNumero
+        
+#     return mdc(segundoNumero, primeiroNumero % segundoNumero)
--- exercicios/operadores_ternarios.py
+++ exercicios/operadores_ternarios.py
@@ -1,6 +1,3 @@
-# %%
-esta_chuvendo = False
 
-print('Hoje estou com as roupas ' + ('secas.', 'molhadas.')[esta_chuvendo])
-print('Hoje estou com as roupas ' + 
-      ('molhadas.' if esta_chuvendo else 'secas.'))
+logged_user = False
+msg = 'usuario logado' if logged_user else 'usuario precisa logar'
