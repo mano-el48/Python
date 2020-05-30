473159491f6eb339263f23933f5e59e79a521853
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-05-30 20:01:08 -0300

    go

--- Recurssao/divisao.py
+++ Recurssao/divisao.py
@@ -1,12 +1,12 @@
 
 divisao = lambda numero1, numero2: 0 if numero1 < numero2  else (1 + divisao(numero1-numero2, numero2))
 
+numero1 = int(input('Digite o primeiro numero: '))
+numero2 = int(input('Digite o segundo numero: '))
+print(divisao(numero1, numero2))
+
 # def divisao(numero1, numero2):
 #     if numero1 < numero2:
 #         return 0
 #     else:
 #         return 1 + divisao(numero1-numero2, numero2)
-
-numero1 = int(input('Digite o primeiro numero: '))
-numero2 = int(input('Digite o segundo numero: '))
-print(divisao(numero1, numero2))
