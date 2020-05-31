04314d30a5c4b753cd2b432dfd7d55c55b2abb6e
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-05-31 00:16:41 -0300

    go

--- Recurssao/fatorial.py
+++ Recurssao/fatorial.py
@@ -1,9 +1,11 @@
-def fatorial(numero):
-     if numero == 0 or numero == 1:
-         return 1
-     else:
-         return numero * fatorial(numero-1)
+
+fatorial = lambda numero: 1 if not numero else numero * fatorial(numero-1)
 
 numero = int(input("Digite o valor: "))
 print(fatorial(numero))
 
+# def fatorial(numero):
+#      if numero == 0:
+#          return 1
+#      else:
+#          return numero * fatorial(numero-1)
