1cbfd524efc8dcefb553e323394553bcdbb6381a
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-05-31 00:20:59 -0300

    go

--- Recurssao/fibonacci.py
+++ Recurssao/fibonacci.py
@@ -1,13 +1,14 @@
-def fib(n):
+
+def fibonacci(n):
     if (not n):#n == 0 
         return 0
 
     elif (n == 1):
         return 1
     
-    return fib(n - 1) + fib(n - 2)
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
 n = int(input("Informe o tamanho da sequencia: "))
-print(fib(n))
+print(fibonacci(n))
 # for i in range(n):
-#   print(fib(i + 1))
+#   print(fibonacci(i + 1))
