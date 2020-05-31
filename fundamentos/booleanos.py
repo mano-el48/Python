b56cc736007bb74c19dda060a67ac5737e32aea9
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-05-30 23:54:39 -0300

    go

--- fundamentos/booleanos.py
+++ fundamentos/booleanos.py
@@ -3,10 +3,10 @@ print('Os falsos.........')
 print(not not 0)
 print(not not None)#parecido com 'null'
 print(not not '')
-print(not not [])
-print(not not ())#tuple
+print(not not [])#lista
+print(not not ())#tupla
 print(not not {})#dicionario
-print(not not set())
+print(not not set())#conjunto
 
 print('------------------')
 
