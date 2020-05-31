5ff5b77d9d41c7aaa209a313966dbffd81d867bb
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-05-30 23:46:04 -0300

    go

---
+++ fundamentos/booleanos.py
@@ -0,0 +1,20 @@
+
+print('Os falsos.........')
+print(not not 0)
+print(not not None)
+print(not not '')
+print(not not [])
+print(not not ())#tuple
+print(not not {})#dicionario
+print(not not set())
+
+print('------------------')
+
+print('Os verdadeiros....')
+print(not not 1)
+print(not not -1)
+print(not not 2)
+print(not not 3)
+print(not not int)
+print(not not float)
+print(not not str)
--- fundamentos/set.py
+++ fundamentos/set.py
@@ -3,6 +3,23 @@ print(type(s))
 s = {1}
 print(type(s))
 
+#adicioando e removendo elementos
+s = set()
+s.add(1)# adiocionado
+print(s)
+s.add(2)
+print(s)
+s.add(2)
+print(s)# não aceita repetições
+s.add(3)
+print(s)
+s.discard(3)# removendo
+print(s)
+s.add('str')
+print(s)
+s.add((4, 5, 6))# adicoando uma tupla
+print(s)
+
 # operacoes
 a = {1, 2, 3, 4}
 b = {3, 4, 5, 6}
