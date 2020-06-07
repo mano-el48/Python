b7642753d2b186f0ad5e99adb4b412125c673525
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-06-06 22:14:54 -0300

    go

---
+++ curso rapido cod3r/estruturas_controle/break_continue.py
@@ -0,0 +1,15 @@
+
+
+for x in range(1, 10):
+    if x % 2 == 0:
+        continue
+    print(x)
+
+print(6 * '-')
+
+for x in range(1, 10):
+    if x == 5:
+        break
+    print(x)
+
+print('Fim!')
---
+++ curso rapido cod3r/estruturas_controle/switch.py
@@ -0,0 +1,14 @@
+def get_dia_semana(dia):
+    dias = {
+        1: 'Domingo',
+        2: 'Segunda',
+        3: 'Terça',
+        4: 'Quarta',
+        5: 'Quinta',
+        6: 'Sexta',
+        7: 'Sábado',
+    }
+    return dias.get(dia, '** Inválido **')
+
+for dia in range(1, 9):
+    print(f'{dia}: {get_dia_semana(dia)}')
