06893337f81cf26ad095b9e3832ac04e02e3b51b
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-06-06 22:29:19 -0300

    go

---
+++ curso rapido cod3r/tipos/booleanos.py
@@ -0,0 +1,20 @@
+
+print('Os falsos.........')
+print(not not 0)
+print(not not None)#parecido com 'null'
+print(not not '')
+print(not not [])#lista
+print(not not ())#tupla
+print(not not {})#dicionario
+print(not not set())#conjunto
+
+print('------------------')
+
+print('Os verdadeiros....')
+print(not not 1)
+print(not not -1)
+print(not not 2)
+print(not not 3)
+print(not not int)
+print(not not float)
+print(not not str)
