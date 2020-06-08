4493b21bf9226521c876468fa0c24b3f8aeab270
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-06-07 23:41:17 -0300

    go

--- funcoes/basico.py
+++ funcoes/basico.py
@@ -1,10 +1,6 @@
-def soma(x: float, y: float) -> float:
+def soma(x , y ):
     return x + y
-
-# print(soma(10, 20))
-# print(soma(20, 20))
-
 
 if __name__ == '__main__':
     print(soma(10, 20))
-print(soma(20, 20))
+    print(soma(20, 20))
