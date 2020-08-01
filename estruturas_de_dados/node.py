9ec0b1ba8784f023f494cd93e6c1f115356b38c4
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-08-01 11:04:34 -0300

    go

--- estruturas_de_dados/node.py
+++ estruturas_de_dados/node.py
@@ -2,3 +2,9 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+# no1 = Node(5)
+# no2 = Node(9)
+# print(no1.data)
+# print(no2.data)
+# print(no2.next)
