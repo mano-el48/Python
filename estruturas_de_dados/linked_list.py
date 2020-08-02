d79dbb36a7eb9d0d0eeac6ed898f8daa79c81ac6
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-08-02 19:17:58 -0300

    go

--- estruturas_de_dados/linked_list.py
+++ estruturas_de_dados/linked_list.py
@@ -46,18 +46,33 @@ class LinkeList:
         else:
             raise IndexError('list index out of range')
 
+    def indexOf(self, el):
+        pointer = self.head
+        i = 0
+        while(pointer):
+            if pointer.data == el:
+                return i
+            pointer = pointer.next
+            i += 1
+
+        raise ValueError('{} is not in the list'.format(el))
+
 
 list = LinkeList()
-print(len(list)) # 0
+print(len(list))  # 0
 
 list.append(7)
-print(len(list)) # 1
-print(list[0]) # 7
+print(len(list))  # 1
+print(list[0])  # 7
 
 list.append(80)
-print(len(list)) # 2
-print(list[1]) # 80
-# print(list[2]) # IndexError('list index out of range')
+print(len(list))  # 2
+print(list[1])  # 80
+# print(list[2])  # IndexError: list index out of range
 
 list[1] = 56
-print(list[1]) # 56
+print(list[1])  # 56
+
+print(list.indexOf(7))
+# print(list.indexOf(80))  # ValueError: 80 is not in the list
+print(list.indexOf(56))
