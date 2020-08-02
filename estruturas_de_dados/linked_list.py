0be1e7c69ac351e79b41a4f8aebe173b9d361e1a
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-08-02 18:43:46 -0300

    go

--- estruturas_de_dados/linked_list.py
+++ estruturas_de_dados/linked_list.py
@@ -1,6 +1,5 @@
 from modules.node import Node
 
-
 class LinkeList:
     def __init__(self):
         self.head = None
@@ -8,12 +7,14 @@ class LinkeList:
 
     def append(self, el):
         if self.head:
+
             # inerção quando a lista ja possui elementos
             pointer = self.head
             while(pointer.next):
                 pointer = pointer.next
             pointer.next = Node(el)
         else:
+
             # primeira inserção
             self.head = Node(el)
         self._length += 1
@@ -22,24 +23,56 @@ class LinkeList:
         return self._length
 
     def get(self, index):
-        pass
+        pointer = self.head
+        for i in range(index):
+            if pointer:
+                pointer = pointer.next
+            else:
+                raise IndexError('list index out of range')
+        if pointer:
+            return pointer.data
+        else:
+            raise IndexError('list index out of range')
 
     def set(self, index, el):
         pass
-    
-    def __getitem__(self, index):
-        pass
-    
-    def __setitem__(self, index):
-        pass
 
+    def __getitem__(self, index):
+        pointer = self.head
+        for i in range(index):
+            if pointer:
+                pointer = pointer.next
+            else:
+                raise IndexError('list index out of range')
+        if pointer:
+            return pointer.data
+        else:
+            raise IndexError('list index out of range')
+
+    def __setitem__(self, index, el):
+        pointer = self.head
+        for i in range(index):
+            if pointer:
+                pointer = pointer.next
+            else:
+                raise IndexError('list index out of range')
+        if pointer:
+            pointer.data = el
+        else:
+            raise IndexError('list index out of range')
 
 
 list = LinkeList()
-print(len(list))
+print(len(list)) # 0
 
 list.append(7)
-print(len(list))
+print(len(list)) # 1
+print(list[0]) # 7
 
 list.append(80)
-print(len(list))
+print(len(list)) # 2
+print(list[1]) # 80
+# print(list[2]) # IndexError('list index out of range')
+
+list[1] = 56
+print(list[1])
