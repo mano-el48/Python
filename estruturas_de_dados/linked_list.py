9a4cd7ab2b24d8f9740cd59b0dc037a347c5e5dd
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-08-02 15:25:31 -0300

    go

--- estruturas_de_dados/linked_list.py
+++ estruturas_de_dados/linked_list.py
@@ -1,4 +1,45 @@
 from modules.node import Node
 
-class LinkeLis:
+
+class LinkeList:
     def __init__(self):
+        self.head = None
+        self._length = 0
+
+    def append(self, el):
+        if self.head:
+            # inerção quando a lista ja possui elementos
+            pointer = self.head
+            while(pointer.next):
+                pointer = pointer.next
+            pointer.next = Node(el)
+        else:
+            # primeira inserção
+            self.head = Node(el)
+        self._length += 1
+
+    def __len__(self):
+        return self._length
+
+    def get(self, index):
+        pass
+
+    def set(self, index, el):
+        pass
+    
+    def __getitem__(self, index):
+        pass
+    
+    def __setitem__(self, index):
+        pass
+
+
+
+list = LinkeList()
+print(len(list))
+
+list.append(7)
+print(len(list))
+
+list.append(80)
+print(len(list))
---
+++ estruturas_de_dados/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+list.append(7)
+# print(len(list))
+
+# list.append(80)
+# print(len(list))
