11b21152852f47ffff86e2040c5714b8f3a589cb
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-08-03 12:28:52 -0300

    go

--- estruturas_de_dados/linked_list.py
+++ estruturas_de_dados/linked_list.py
@@ -6,14 +6,6 @@ class LinkedList:
         self._head = None
         self._length = 0
 
-    @property
-    def head(self):
-        pointer = self._head
-        if pointer:
-            return pointer.data
-        else:
-            raise ValueError('the list is empty')
-
     def append(self, el):
         if self._head:
             # inerção quando a lista ja possui elementos
@@ -21,6 +13,7 @@ class LinkedList:
             while(pointer.next):
                 pointer = pointer.next
             pointer.next = Node(el)
+
         else:
             # primeira inserção
             self._head = Node(el)
@@ -31,8 +24,6 @@ class LinkedList:
         for i in range(index):
             if pointer:
                 pointer = pointer.next
-            else:
-                raise IndexError('list index out of range')
         return pointer
 
     def __len__(self):
@@ -89,7 +80,6 @@ print(list[0])  # 7
 list.append(80)
 print(len(list))  # 2
 print(list[1])  # 80
-# print(list[2])  # IndexError: list index out of range
 
 list[1] = 56
 print(list[1])  # 56
@@ -97,12 +87,10 @@ print(list[1])  # 56
 print(list.index(7))  # 0
 # print(list.index(80))  # ValueError: 80 is not in the list
 print(list.index(56))  # 1
-print(list.head)  # 7
 
 list.clear()
 print(len(list))  # 0
 # print(list[0]) # list index out of range
-# print(list.head)  # ValueError: the list is empty
 
 list.append(7)
 list.append(80)
@@ -111,6 +99,6 @@ list.append(32)
 list.append(17)
 
 list.insert(0, 22)
-print(list[0]) # 22
-print(list[1]) # 7
-print(len(list))
+print(list[0])  # 22 -> head
+print(list[len(list) - 1]) # 17
+print(list[10]) # IndexError: list index out of range
