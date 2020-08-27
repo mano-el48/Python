2da67904bd9eae0156b37dd08eb785b25246ea94
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-08-27 17:39:09 -0300

    go

--- exercises/date.py
+++ exercises/date.py
@@ -5,7 +5,8 @@ date = datetime(1998, 10, 9)
 date = date.strftime('%d/%m/%Y')  # passando para string
 print(date)
 
-date = datetime.strptime('09/10/1998', '%d/%m/%Y')
+date_str = '09/10/1998'
+date = datetime.strptime(date_str, '%d/%m/%Y')
 print(date)
 print(date.day)
 print(date.month)
