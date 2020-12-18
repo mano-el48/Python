c8da6ff809e4fced2e98c37cc62b1156c0582842
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2020-12-18 12:47:17 -0300

    go

--- data_structures_and_algorithms/main-lists.py
+++ data_structures_and_algorithms/main-lists.py
@@ -1,45 +1,224 @@
 from lists.linked_list import LinkedList
 
 list = LinkedList()
-list.append(7)
-print(len(list))  # 1
-# print(list)  # IndexError: the lis is empty
 
-list.append(80)
-print(len(list))  # 2
-print(list[0])  # 7
+while True:
+    print('Menu de opcoes:')
+    print('1  - Inserir elemento inicio')
+    print('2  - Inserir elemento final')
+    print('3  - Inserir elemento (ordenado)')
+    print('4  - Inserir elemento (qualquer posicao)')
+    print('5  - Remover elemento inicio')
+    print('6  - Remover elemento final')
+    print('7  - Remover elemento (qualquer)')
+    print('8  - Obter quantidade de elementos na lista')
+    print('9  - Obter elemento pelo indice')
+    print('10 - Obter indice pelo elemento')
+    print('11 - Alterar elemento')
+    print('12 - Impirmir lista')
+    print('13 - Limpar lista')
+    option = input('Opcao:')
+    
+    if option == 1:
+        el = int(input('\nDigite o elemento a ser inserido: '))
+        list.insert(el)
+        
+        print(f'\n{list}')
 
-list[1] = 56
-print(list[0])  # 7
-print(list[1])  # 56
-print(len(list))  # 2
+    elif option == 2:
+       el = int(input('\nDigite o elemento a ser inserido: '))
 
-print(list.index(7))  # 0
-# print(list.index(80))  # ValueError: 80 is not in the list
-print(list.index(56))  # 1
+       list.push(el)
+       print(f'\n{list}')
 
-list.clear()
-print(len(list))  # 0
-# print(list[0]) # list index out of range
+    elif option == 3:
 
-list.append(7)
-list.append(80)
-list.append(56)
-list.append(32)
-list.append(17)
-print(list) # 7 -> 80 -> 56 -> 32 -> 17 ->
+       el = int(input('\nDigite o elemento a ser inserido: '))
+       list.sortedInsert(el)
+   
+       print(f'\n{list}')
 
-list.insert(0, 22)
-print(list[0])  # 22
-print(list[len(list) - 1])  # 17
-# print(list[10]) # IndexError: list index out of range
+    elif option == 4:
 
-print(list) # 22 -> 7 -> 80 -> 56 -> 32 -> 17 ->
-list.remove(22)
-list.remove(7)
-print(list)  # 80 -> 56 -> 32 -> 17 ->
-list.remove(80)
-list.remove(56)
-list.remove(32)
-list.remove(17)
-# print(list) # IndexError: the list is empty
+       el = int(input('\nDigite o elemento a ser inserido: '))
+       index = int(input('\nDigite o indice do novo elemento: '))
+       
+        if (list.insert(el, index) != = undefined) {
+            print(f'\n{list}')
+
+        } else {
+            console.log(`\nFalha na insercao!`)
+
+        }
+
+elif option == 5:
+
+        if (!list.isEmpty()) {
+            list.removeAt(0)
+
+            if (list.isEmpty()) {
+                console.log('\nA lista esta vazia!')
+
+            } else {
+                print(f'\n{list}')
+            }
+
+
+        } else {
+            console.log('\nA lista esta vazia!')
+        }
+
+elif option == 6:
+
+        if (!list.isEmpty()) {
+            list.pop()
+
+            if (list.isEmpty()) {
+                console.log('\nA lista esta vazia!')
+
+            } else {
+                print(f'\n{list}')
+            }
+
+        } else {
+            console.log('\nA lista esta vazia!')
+        }
+
+elif option == 7:
+
+        if (!list.isEmpty()) {
+            el = questionInt('\nDigite o elemento a ser removido: ')
+            index = list.indexOf(el)
+
+            if (index != = -1) {
+                list.removeAt(index)
+
+                if (list.isEmpty()) {
+                    console.log('\nA lista esta vazia!')
+
+                } else {
+                    print(f'\n{list}')
+                }
+
+            } else {
+                console.log('\nElemento nao encontrado!')
+            }
+
+        } else {
+            console.log('\nA lista esta vazia!')
+
+        }
+
+elif option == 8:
+
+        if (list.isEmpty()) {
+            console.log('\nA lista esta vazia!')
+
+        } else {
+
+            console.log(`\nQuantidade de elementos na lista: ${list.length}`)
+        }
+
+elif option == 9:
+
+        index = questionInt("\nDigite o indice: ")
+        el = list.getElementAt(index)
+
+        if (el != = undefined) {
+            console.log(`\nElemento na posicao ${index}: ${el}`)
+
+        } else {
+            console.log('\nElemento nao encontrado!')
+        }
+
+elif option == 1: {
+
+        el = questionInt("\nDigite o elemento: ")
+        index = list.indexOf(el)
+
+        if (list.indexOf(el) != = -1) {
+            console.log(`\nElemento na posicao ${index}: ${el}`)
+
+        } else {
+            console.log('\nElemento nao encontrado!')
+        }
+
+elif option == 1: {
+
+        index = questionInt("\nDigite o indice do elemento a ser alterado: ")
+        el = questionInt("Digite o novo elemento: ")
+
+        list.setElementAt(el, index)
+
+        if (list.getElementAt(index) == = el) {
+            console.log(`\nAlteracao realizada com sucesso!\n`)
+            console.log(list.toString())
+
+        } else {
+            console.log(`\nHouve um erro na alteracao!`)
+        }
+
+elif option == 1: {
+
+        if (list.isEmpty()) {
+            console.log('\nA lista esta vazia!');} else {
+            print(f'\n{list}')
+
+        }
+
+elif option == 1: {
+
+        list.clear()
+
+        if (list.isEmpty()) {
+            console.log('\nA lista esta vazia!')
+        }
+
+    } else {
+        console.log("\nOpcao Invalida!")
+    }
+
+} while (1)
+
+# list.append(7)
+# print(len(list))  # 1
+# # print(list)  # IndexError: the lis is empty
+
+# list.append(80)
+# print(len(list))  # 2
+# print(list[0])  # 7
+
+# list[1] = 56
+# print(list[0])  # 7
+# print(list[1])  # 56
+# print(len(list))  # 2
+
+# print(list.index(7))  # 0
+# # print(list.index(80))  # ValueError: 80 is not in the list
+# print(list.index(56))  # 1
+
+# list.clear()
+# print(len(list))  # 0
+# # print(list[0]) # list index out of range
+
+# list.append(7)
+# list.append(80)
+# list.append(56)
+# list.append(32)
+# list.append(17)
+# print(list) # 7 -> 80 -> 56 -> 32 -> 17 ->
+
+# list.insert(0, 22)
+# print(list[0])  # 22
+# print(list[len(list) - 1])  # 17
+# # print(list[10]) # IndexError: list index out of range
+
+# print(list) # 22 -> 7 -> 80 -> 56 -> 32 -> 17 ->
+# list.remove(22)
+# list.remove(7)
+# print(list)  # 80 -> 56 -> 32 -> 17 ->
+# list.remove(80)
+# list.remove(56)
+# list.remove(32)
+# list.remove(17)
+# # print(list) # IndexError: the list is empty
